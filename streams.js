887b44de3f99485fdc783e6a9aa447994f31e369
aiorbd-video <ratulhasan5a@gmail.com>
2025-12-10 22:24:17 +0600

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,5 +1,3 @@
 {
   "dormemon": "https://cloudfrontnet.vercel.app/tplay/playout/209622/master.m3u8",
-  "ten_cricket": "https://allottplay.com/tv/toffee/live.php?id=ten_cricket&e=.m3u8",
-  "gazibdz": "https://app24.jagobd.com.bd/.../gazibdz.stream/tracks-v1a1/mono.m3u8"
-}
+    }

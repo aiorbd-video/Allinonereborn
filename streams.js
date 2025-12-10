d773c62da6e0802276daf70ff0c7b17de02eb154
aiorbd-video <ratulhasan5a@gmail.com>
2025-12-10 21:54:50 +0600

    Create streams.js

---
+++ streams.js
@@ -0,0 +1,5 @@
+{
+  "dormemon": "https://cloudfrontnet.vercel.app/tplay/playout/209902/master.m3u8",
+  "ten_cricket": "https://allottplay.com/tv/toffee/live.php?id=ten_cricket&e=.m3u8",
+  "gazibdz": "https://app24.jagobd.com.bd/.../gazibdz.stream/tracks-v1a1/mono.m3u8"
+}
